1421a788a3774a302eddd97ef61ade02f3a76e4d
Wilson Wong <wilwong89@gmail.com>
2020-01-20 20:10:51 +0000

    updated tinyURL express app

--- express_server.js
+++ express_server.js
@@ -1,15 +1,49 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const path = require('path');
+// const path = require('path');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function() {
+  const length = 6;
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(urlDatabase)
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.redirect("/urls");
+  }
+});
+
 app.set("view engine", "ejs");
 //app.set('views', path.join(__dirname, 'views'));
+
+app.post("/urls", (req, res) => {
+  let newKey = generateRandomString();
+  urlDatabase[newKey] = req.body.longURL;
+  console.log(req.body);  // Log the POST request body to the console
+  res.redirect("/urls");  // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
