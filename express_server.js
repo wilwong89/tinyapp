1ed824e83b6392f115ab0d0d3b160ee33d3b64ce
Wilson Wong <wilwong89@gmail.com>
2020-01-20 19:12:47 +0000

    Updated shortURL app

---
+++ express_server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const path = require('path');
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.set("view engine", "ejs");
+//app.set('views', path.join(__dirname, 'views'));
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
